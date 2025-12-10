c9a7b053bcdd0cef10ac097831319a2a27065199
chihab21 <chihabghlala49@gmail.com>
2025-12-10 23:27:20 +0100

    chiheb

---
+++ index.js
@@ -0,0 +1,18 @@
+
+ const Hello=()=>{
+    console.log('heelo');
+    
+} 
+let name="chiheb"
+for(let i=0; i<= 10;i++){
+
+    console.log( "i Love Tasnim ");
+    
+
+}
+
+
+Hello()
+
+
+console.log(name);
